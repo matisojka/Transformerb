97eb68b225ef5f33dea448a4ecb46568147d00a9
Mateusz Sojka <mateusz.sojka@infopark.de>
2012-03-23 07:37:43 +0100

    Etl specs green

--- spec/lib/transformerb/etl_spec.rb
+++ spec/lib/transformerb/etl_spec.rb
@@ -14,19 +14,38 @@ describe Transformerb::Etl do
         'region'            => nil
       }
 
-      setup =<<-EOF
+      @setup =<<-EOF
         fields do
           take 'name'
           take 'Last name'
         end
         EOF
 
-      @etl = Transformerb::Etl.new(@data, setup)
+      @etl = Transformerb::Etl.new(@data, @setup)
     end
 
     describe '#fields' do
+      before do
+        class DummyExtractor
+
+          def initialize
+            @value = { 'name' => 'Lionel', 'Last name' => 'Messi' }
+          end
+
+          def next
+            return_value = @value
+            @value = nil
+
+            return_value
+          end
+        end
+
+        @etl.extractor = DummyExtractor.new
+        @etl.loader.stub!(:write).and_return nil
+      end
+
       it 'sets the import_attributes correctly' do
-        pending 'TODO => stub extractor and loader'
+        @etl.run
 
         @etl.import_attributes[:name].should == 'Lionel'
         @etl.import_attributes[:last_name].should == 'Messi'
@@ -112,6 +131,13 @@ describe Transformerb::Etl do
 
       it 'writes the result of the transformation' do
         @etl.run
+
+        csv_file_content = File.open('spec/lib/transformerb/adapters/output_spec.csv').read
+        csv_file_content.should == "Andres,Iniesta\nVictor,Valdes\n"
+      end
+
+      after do
+        system('rm spec/lib/transformerb/adapters/output_spec.csv')
       end
     end
 
