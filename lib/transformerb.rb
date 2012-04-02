65cd93f5e0ca422399809c6f8d8267d40e48f1c9
Mateusz Sojka <mateusz.sojka@infopark.de>
2012-04-02 21:10:11 +0200

    Added executable for an easier use of Transformerb

--- lib/transformerb.rb
+++ lib/transformerb.rb
@@ -1,9 +1,12 @@
-require 'transformerb/etl.rb'
-require 'transformerb/field_transformer.rb'
-require 'transformerb/adapters/csv_adapter.rb'
+require File.join(File.dirname(__FILE__), 'transformerb/etl.rb')
+require File.join(File.dirname(__FILE__), 'transformerb/field_transformer.rb')
+require File.join(File.dirname(__FILE__), 'transformerb/adapters/csv_adapter.rb')
 
+require 'rubygems'
 require 'active_support/inflector'
 
 require 'date'
 require 'pry'
+
+# TODO: Use FasterCSV under Ruby 1.8.7
 require 'csv'
